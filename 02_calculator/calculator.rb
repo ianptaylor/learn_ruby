bf93cc541a3b5a3aa5fbb5fa601c5c1736c344c4
Ian Taylor <ianptaylor@gmail.com>
2017-07-29 15:16:10 -0500

    Add calculator with BONUS

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
 #write your code here
+def add (foo, bar)
+  foo + bar
+end
+
+def subtract (foo, bar)
+  foo - bar
+end
+
+def sum (arr)
+  ret = 0
+  arr.each { |x| ret = ret + x }
+  return ret
+end
+
+def multiply (arr)
+  ret = 1
+  arr.each { |x| ret = ret * x }
+  return ret
+end
+
+def power (foo, bar)
+  foo ** bar
+end
+
+def factorial (foo)
+  if foo == 0
+    1
+  elsif foo == 1
+    1
+  else
+    foo * factorial(foo - 1)
+  end
+end
