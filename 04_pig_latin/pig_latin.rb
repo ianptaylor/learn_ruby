7bac4d53a11331727837e050c01d74ceed3074be
Ian Taylor <ianptaylor@gmail.com>
2017-07-30 09:14:28 -0500

    Regex magic

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,9 @@
 #write your code here
+def translate (words)
+  words.split.map do |word|
+    word =~ /^([^aeiouyq]*(qu)?)(.*)$/x
+    first = $1
+    rest = $3
+    "#{rest}#{first}ay"
+  end.join(" ")
+end
