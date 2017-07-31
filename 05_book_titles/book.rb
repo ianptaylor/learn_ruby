9cc87a75cf21b9fe5ca6f692fadf46cd9f97b1ec
Ian Taylor <ianptaylor@gmail.com>
2017-07-30 21:48:50 -0500

    Book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
 # write your code here
+  attr_reader :title
+
+  def title= (foo)
+    small_words = %w[in a an on the and or of over]
+
+    words = foo.split(' ').map do |word|
+      small_words.include?(word) ? word : word.capitalize
+    end
+    words.first.capitalize!
+
+    @title = words.join(' ')
+  end
 end
