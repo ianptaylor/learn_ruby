3384710022e7ac7042ff367e2fc901e491ea9904
Ian Taylor <ianptaylor@gmail.com>
2017-07-30 21:57:54 -0500

    Use / and % for minutes

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,14 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize (time = 0)
+    @seconds = time
+  end
+
+  def time_string
+    h = @seconds.to_i / 3600
+    m = (@seconds.to_i / 60) % 60
+    s = @seconds % 60
+    "#{h.to_s.rjust(2, '0')}:#{m.to_s.rjust(2, '0')}:#{s.to_s.rjust(2, '0')}"
+  end
 end
